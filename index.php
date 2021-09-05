86ed468ba1273c8c4705b64f183ed7867ac07231
sakura <57336835+15680867951@users.noreply.github.com>
2021-09-05 17:18:03 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>朗朗医疗投资有限公司</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <link rel="shortcut icon" href="favicon.ico">
+    <link href="./font/iconfont/iconfont.css" rel="stylesheet">
+    <link href="./css/sassConfig.css" rel="stylesheet">
+    <link href="./css/swiper-bundle.min.css" rel="stylesheet">
+    <link href="./css/normalize.css" rel="stylesheet">
+    <link href="./css/common.css" rel="stylesheet">
+    <link href="./css/main.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="container">
+        <header class="clearfix">
+            <h1 class="logo pull-left  ">
+                <a href="index.html" title="朗朗口腔" target="_blank">
+                    <img src="./images/logo.png" alt="朗朗口腔">
+                </a>
+            </h1>
+            <div class="pull-right top-menu">
+                <div class="pull-right">
+                    <div class="menu-link">
+                        <div class="lang">
+                            中文
+                            <ul>
+                                <a href="javascript:void(0);">中文</a>
+                                <a href="javascript:void(0);">英文</a>
+                            </ul>
+                        </div><span class="link">|</span>
+                        <a href="tel:0755-86566573" title="0755-86566573">0755-86566573</a><span class="link">|</span>
+                        <a href="index.html" title="朗朗口腔集团官网">朗朗口腔集团官网</a>
+
+                    </div>
+                    <nav>
+                        <ul>
+                            <li class="current">
+                                <a href="javascript:void(0);" title="首页">首页</a>
+                                <ul>
+                                    <li>
+                                        <a href="javascript:void(0);" title="朗朗简介">朗朗简介</a>
+                                        <a href="javascript:void(0);" title="朗朗优势">朗朗优势</a>
+                                        <a href="javascript:void(0);" title="核心团队">核心团队</a>
+                                        <a href="javascript:void(0);" title="朗朗历程">朗朗历程</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0);" title="走进朗朗">走进朗朗</a>
+                                <ul>
+                                    <li>
+                                        <a href="javascript:void(0);" title="朗朗简介">朗朗简介</a>
+                                        <a href="javascript:void(0);" title="朗朗优势">朗朗优势</a>
+                                        <a href="javascript:void(0);" title="核心团队">核心团队</a>
+                                        <a href="javascript:void(0);" title="朗朗历程">朗朗历程</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0);" title="咨询动态">咨询动态</a>
+                                <ul>
+                                    <li>
+                                        <a href="javascript:void(0);" title="最新动态">最新动态</a>
+                                        <a href="javascript:void(0);" title="强强联手">强强联手</a>
+                                        <a href="javascript:void(0);" title="党支部风采">党支部风采</a>
+                                        <a href="javascript:void(0);" title="媒体报道">媒体报道</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0);" title="合作伙伴">合作伙伴</a>
+                                <ul>
+                                    <li>
+                                        <a href="javascript:void(0);" title="精品门店">精品门店</a>
+                                        <a href="javascript:void(0);" title="统企合作">统企合作</a>
+                                        <a href="javascript:void(0);" title="校企合作">校企合作</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0);" title="加入朗朗">加入朗朗</a>
+                                <ul>
+                                    <li>
+                                        <a href="javascript:void(0);" title="门诊合伙">门诊合伙</a>
+                                        <a href="javascript:void(0);" title="个人应聘">个人应聘</a>
+                                        <a href="javascript:void(0);" title="在线留言">在线留言</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    </div>
+    <!-- 幻灯片start -->
+    <div id="slide-wrap">
+        <div class="slide-content">
+            <ul class="clearfix">
+                <li><img src="./images/1.jpeg" alt="轮播图"></li>
+                <li><img src="./images/2.jpeg" alt="轮播图"></li>
+                <li><img src="./images/3.jpeg" alt="轮播图"></li>
+                <li><img src="./images/4.jpeg" alt="轮播图"></li>
+            </ul>
+        </div>
+        <ul class="slide-nav">
+            <li><a href="javascript: void(0)" class="current"></a></li>
+            <li><a href="javascript: void(0)"></a></li>
+            <li><a href="javascript: void(0)"></a></li>
+            <li><a href="javascript: void(0)"></a></li>
+        </ul>
+        <span class="slide-prev"><i class="iconfont icon-jiantou-copy"></i></span>
+        <span class="slide-next"><i class="iconfont icon-arrow-right"></i></span>
+    </div>
+    <!-- 幻灯片end -->
+
+    <!-- 朗朗口腔start -->
+    <div class="container">
+        <section>
+            <header>
+                <div class="main-column padding-top-35 padding-bottom-48">
+                    <h3>朗朗口腔</h3>
+                    <p>呵护牙齿健康 创造美好未来<span></span>CARTE FOR TEETH TO CREATE A BETTER FUTURE</p>
+                </div>
+            </header>
+            <div class="info-list">
+                <div id="infoListWrap">
+                    <div class="loading-wait">
+                        <div class="rotate">
+                            <i class="iconfont icon-jiazai"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <a class="more" href="javascript:void(0);" title="查看更多">查看更多</a>
+        </section>
+        <!-- 朗朗口腔end-->
+        <!-- 朗朗公益start -->
+        <section class="welfare">
+            <header>
+                <div class="main-column padding-top-5 padding-bottom-28">
+                    <h3>朗朗公益</h3>
+                    <p>呵护牙齿健康 创造美好未来<span></span>CARTE FOR TEETH TO CREATE A BETTER FUTURE</p>
+                </div>
+            </header>
+            <div class="welfare-wrap" id="welfareWrap">
+            </div>
+        </section>
+        <!-- 朗朗公益end -->
+    </div>
+    <!-- 朗朗医生start -->
+    <div class="doctor">
+        <div class="container clearfix">
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="img">
+                            <img src="./images/自然风景背景图 (4).jpg">
+                            <span class="name">杨苏红</span>
+                        </div>
+                        <div class="doc-info">
+                            <h4 class="title">牙周主诊医生</h4>
+                            <span class="relif">擅长：仿真陶瓷牙修复、牙周系统治疗、残根残冠的保存治疗等。</span>
+                            <i class="line"></i>
+                            <p class="dec">国内知名大学口腔医学学士，曾就职于高端私人齿科工作室，从事口腔事业10余年，对口腔各类常见疾病的临床治疗积累了丰富经验。积极学习新技术的临床应用，不断进修提升，精通牙体保存治疗。数次参加北京医科大学牙周学习研究项目、香港大学牙周学习研究项目。技术上追求完美，坚持“微创无痛治疗”原则，深受患者的信赖！</p>
+                            <a href="javascript:void(0);" class="link-more">
+                                查看更多
+                                <i class="iconfont icon-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="img">
+                            <img src="./images/自然风景背景图 (5).jpg">
+                            <span class="name">陈芋龙</span>
+                        </div>
+                        <div class="doc-info">
+                            <h4 class="title">牙周主诊医生</h4>
+                            <span class="relif">擅长：仿真陶瓷牙修复、牙周系统治疗、残根残冠的保存治疗等。</span>
+                            <i class="line"></i>
+                            <p class="dec">国内知名大学口腔医学学士，曾就职于高端私人齿科工作室，从事口腔事业10余年，对口腔各类常见疾病的临床治疗积累了丰富经验。积极学习新技术的临床应用，不断进修提升，精通牙体保存治疗。数次参加北京医科大学牙周学习研究项目、香港大学牙周学习研究项目。技术上追求完美，坚持“微创无痛治疗”原则，深受患者的信赖！</p>
+                            <a href="javascript:void(0);" class="link-more">
+                                查看更多
+                                <i class="iconfont icon-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="img">
+                            <img src="./images/自然风景背景图 (6).jpg">
+                            <span class="name">杨苏红</span>
+                        </div>
+                        <div class="doc-info">
+                            <h4 class="title">牙周主诊医生</h4>
+                            <span class="relif">擅长：仿真陶瓷牙修复、牙周系统治疗、残根残冠的保存治疗等。</span>
+                            <i class="line"></i>
+                            <p class="dec">国内知名大学口腔医学学士，曾就职于高端私人齿科工作室，从事口腔事业10余年，对口腔各类常见疾病的临床治疗积累了丰富经验。积极学习新技术的临床应用，不断进修提升，精通牙体保存治疗。数次参加北京医科大学牙周学习研究项目、香港大学牙周学习研究项目。技术上追求完美，坚持“微创无痛治疗”原则，深受患者的信赖！</p>
+                            <a href="javascript:void(0);" class="link-more">
+                                查看更多
+                                <i class="iconfont icon-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="swiper-slide">
+                        <div class="img">
+                            <img src="./images/自然风景背景图 (7).jpg">
+                            <span class="name">陈芋龙</span>
+                        </div>
+                        <div class="doc-info">
+                            <h4 class="title">牙周主诊医生</h4>
+                            <span class="relif">擅长：仿真陶瓷牙修复、牙周系统治疗、残根残冠的保存治疗等。</span>
+                            <i class="line"></i>
+                            <p class="dec">国内知名大学口腔医学学士，曾就职于高端私人齿科工作室，从事口腔事业10余年，对口腔各类常见疾病的临床治疗积累了丰富经验。积极学习新技术的临床应用，不断进修提升，精通牙体保存治疗。数次参加北京医科大学牙周学习研究项目、香港大学牙周学习研究项目。技术上追求完美，坚持“微创无痛治疗”原则，深受患者的信赖！</p>
+                            <a href="javascript:void(0);" class="link-more">
+                                查看更多
+                                <i class="iconfont icon-arrow-right"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Add Pagination -->
+            </div>
+            <div class="p-r">
+                <div class="swiper-pagination doc-dots p-a"></div>
+            </div>
+        </div>
+    </div>
+    <!-- 朗朗医生end -->
+    <!-- 资讯动态start -->
+    <div class="container">
+        <section>
+            <header>
+                <div class="main-column padding-top-20 padding-bottom-30">
+                    <h3>资讯动态</h3>
+                    <p>呵护牙齿健康 创造美好未来<span></span>CARTE FOR TEETH TO CREATE A BETTER FUTURE</p>
+                </div>
+            </header>
+            <!-- indexNewsAjax start-->
+            <ul class="tab-menu">
+            </ul>
+            <!-- indexNewsAjax end-->
+            <div id="tab-content-wrap">
+                <div id="industry"></div>
+                <!--朗朗资讯-->
+                <div id="information" style="display: none;"></div>
+                <!--媒体报道-->
+                <div id="media" style="display: none;"></div>
+                <!--公益活动-->
+                <div id="welfare" style="display: none;"></div>
+            </div>
+        </section>
+    </div>
+    <!-- 资讯动态end -->
+    <footer>
+        <div class="container">
+            <div class="clearfix">
+                <div class="pull-left">
+                    <address class="footer-contact-info">
+                        <h5>朗朗医疗投资有限公司</h5>
+                        <p>集团总部热线：<a href="4000-890-608" title="4000-890-608">4000-890-608</a></p>
+                        <p>集团总部地址：广东省深圳市南山区高新南一道3号富诚大厦三楼</p>
+                    </address>
+                    <ul class="share clearfix">
+                        <a href="javascript:void(0);" class="sing-share">
+                            <i class="iconfont icon-weibo1"></i>
+                        </a>
+                        <a href="javascript:void(0);" class="wechat-share">
+                            <i class="iconfont icon-weixin"></i>
+                            <p><img src="./images/二维码.png" alt="二维码"></p>
+                        </a>
+                    </ul>
+                </div>
+                <div class="pull-right">
+                    <ul class="footer-menu clearfix">
+                        <dl>
+                            <dt><a href="" title="走进朗朗">走进朗朗</a></dt>
+                            <dd><a href="" title="朗朗简介">朗朗简介</a></dd>
+                            <dd><a href="" title="朗朗优势">朗朗优势</a></dd>
+                            <dd><a href="" title="企业荣誉">企业荣誉</a></dd>
+                            <dd><a href="" title="团队风采">团队风采</a></dd>
+                        </dl>
+                        <dl>
+                            <dt><a href="" title="资讯动态">资讯动态</a></dt>
+                            <dd><a href="" title="行业动态">行业动态</a></dd>
+                            <dd><a href="" title="朗朗资讯">朗朗资讯</a></dd>
+                            <dd><a href="" title="媒体报道">媒体报道</a></dd>
+                            <dd><a href="" title="公益活动">公益活动</a></dd>
+                        </dl>
+                        <dl>
+                            <dt><a href="" title="合作伙伴">合作伙伴</a></dt>
+                            <dd><a href="" title="门诊列表">门诊列表</a></dd>
+                            <dd><a href="" title="交通指引">交通指引</a></dd>
+                        </dl>
+                        <dl>
+                            <dt><a href="" title="辅助功能">辅助功能</a></dt>
+                            <dd><a href="" title="公众号指引">公众号指引</a></dd>
+                            <dd><a href="" title="交通指南">交通指南</a></dd>
+                            <dd><a href="" title="在线客服">在线客服</a></dd>
+                            <dd><a href="" title="一键拨号">一键拨号</a></dd>
+                        </dl>
+                        <dl>
+                            <dt><a href="" title="联系我们">联系我们</a></dt>
+                            <dd><a href="" title="联系我们">联系我们</a></dd>
+                            <dd><a href="" title="加入我们">加入我们</a></dd>
+                            <dd><a href="" title="在线留言">在线留言</a></dd>
+                        </dl>
+                        <dl>
+                            <img src="/images/二维码.png" alt="二维码">
+                            <p class="text-center">微信公众号</p>
+                        </dl>
+                    </ul>
+                </div>
+            </div>
+            <div id="copyright">@ 2005-2016 朗朗口腔医疗投资有限公司 版权所有 粤ICP备08130115号-1</div>
+        </div>
+    </footer>
+    <script src="https://cdn.staticfile.org/jquery/3.6.0/jquery.js"></script>
+    <script src="./js/banner.js"></script>
+    <script src="./js/common.js"></script>
+    <script src="./js/swiper-bundle.min.js"></script>
+    <script src="./js/doctorSwiper.js"></script>
+    <script src="./js/indexNewsAjax.js"></script>
+    <script src="./js/kouqiangAjax.js"></script>
+    <script src="./js/gongyiAjax.js"></script>
+</body>
+
+</html>
