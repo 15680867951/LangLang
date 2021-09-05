f80e0a30258b157979c280e41e37924dd16c4e01
sakura <57336835+15680867951@users.noreply.github.com>
2021-09-05 17:27:05 +0800

    Delete js directory

--- js/common.js
+++
@@ -1,9 +0,0 @@
-function loadingMsg(data){
-    let wrap=document.getElementById('infoListWrap');
-    wrap.innerHTML=`<div class="loading-wait">
-                        <p>${data.message}</p
-                        <div class="rotate">
-                            <i class="iconfont icon-jiazai"></i>
-                        </div>                
-                    </div>`
-}
